ec32445cc813c9b3b044e230afcdaa30b44bf3e9
Julian Jalsovsky <julian.jalsovsky@gmail.com>
2023-09-26 19:56:58 +0200

    Add allowedOrigins and allowedMethods to WebConfig

--- src/main/java/sk/jalsovsky/warehouse/config/WebConfig.java
+++ src/main/java/sk/jalsovsky/warehouse/config/WebConfig.java
@@ -1,4 +1,4 @@
-package sk.jalsovsky.warehouse;
+package sk.jalsovsky.warehouse.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://127.0.0.1:3000" , "http://localhost:3000")
+                .allowedMethods("GET");
     }
 }
