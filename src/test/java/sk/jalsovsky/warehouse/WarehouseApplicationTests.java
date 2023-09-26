a9ec93f3908ba489dde5603dd03bbfe33b211529
Julian Jalsovsky <julian.jalsovsky@gmail.com>
2023-09-26 13:20:20 +0200

    Initial commit

---
+++ src/test/java/sk/jalsovsky/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.jalsovsky.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
